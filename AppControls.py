76db3f3519fd2c26c24925ab35fb3e065f8e61ca
Filip Axani <filip.axani@gmail.com>
2014-09-15 11:03:29 +0200

    Use *args and **kwargs for button-subclass

--- AppControls.py
+++ AppControls.py
@@ -1,11 +1,6 @@
 import wx
 
 class myButtonSubclass(wx.Button):
-	def __init__(self, parent,
-			id = wx.ID_ANY, 
-			label = u"ButtonLabel", 
-			pos = wx.DefaultPosition, 
-			size = wx.DefaultSize, 
-			proportion = 0):
-		wx.Button.__init__(self, parent, id, label, pos, size, proportion)
+	def __init__(self, *args, **kwargs):
+		wx.Button.__init__(self, *args, **kwargs)
 		print 'Button loaded'
