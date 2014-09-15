177ac8d3acc4a5a36f9818417dbee56392901916
Filip Axani <filip.axani@gmail.com>
2014-09-15 10:03:06 +0200

    Add wxFormbuilder file

---
+++ gui.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*- 
+
+###########################################################################
+## Python code generated with wxFormBuilder (version Jun  5 2014)
+## http://www.wxformbuilder.org/
+##
+## PLEASE DO "NOT" EDIT THIS FILE!
+###########################################################################
+
+import wx
+import wx.xrc
+
+###########################################################################
+## Class simpleWXApp-window
+###########################################################################
+
+class simpleWXApp-window ( AppControl ):
+	
+	def __init__( self, parent ):
+		AppControl.__init__ ( self, parent, id = wx.ID_ANY, title = wx.EmptyString, pos = wx.DefaultPosition, size = wx.Size( 193,147 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
+		
+		self.SetSizeHintsSz( wx.DefaultSize, wx.DefaultSize )
+		
+		boxSizer = wx.BoxSizer( wx.VERTICAL )
+		
+		self.myButton = wx.Button( self, wx.ID_ANY, u"ButtonLabel", wx.DefaultPosition, wx.DefaultSize, 0 )
+		boxSizer.Add( self.myButton, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL, 5 )
+		
+		
+		self.SetSizer( boxSizer )
+		self.Layout()
+		
+		self.Centre( wx.BOTH )
+		
+		# Connect Events
+		self.myButton.Bind( wx.EVT_BUTTON, self.buttonAction )
+	
+	def __del__( self ):
+		pass
+	
+	
+	# Virtual event handlers, overide them in your derived class
+	def buttonAction( self, event ):
+		event.Skip()
+	
+
